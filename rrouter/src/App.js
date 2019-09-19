7c732afcacb92bbd10d60756faf92ec05dba5dd1
NGUYEN XUAN HUY p1419771 <23189942+nxhuy-github@users.noreply.github.com>
2019-09-19 10:43:23 +0200

    use Switch, prop 'exact', Link

--- rrouter/src/App.js
+++ rrouter/src/App.js
@@ -10,11 +10,22 @@ function App() {
     <Router>  
       <div>
         <Nav />
-        <Route path="/about" component={About} />
-        <Route path="/shop" component={Shop} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
       </div>
     </Router>
   );
+}
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
 }
 
 export default App;
--- rrouter/src/components/Nav.js
+++ rrouter/src/components/Nav.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 function Nav() {
   return (
     <nav>
       <h3>Logo</h3>
       <ul className="nav-links">
-        <li>About</li>
-        <li>Shop</li>
+        <Link to="/about" >
+          <li>About</li>
+        </Link>
+        <Link to="/shop" >
+          <li>Shop</li>
+        </Link>
       </ul>
     </nav>
   );
