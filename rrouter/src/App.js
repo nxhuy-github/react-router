685ee027c7909ba70b3376e5b8d304eaeffc8cd0
NGUYEN XUAN HUY p1419771 <23189942+nxhuy-github@users.noreply.github.com>
2019-09-19 10:33:50 +0200

    use wrapper Router, use component Route for routing

--- rrouter/src/App.js
+++ rrouter/src/App.js
@@ -3,14 +3,17 @@ import Nav from './components/Nav'
 import About from './components/About'
 import Shop from './components/Shop'
 import './App.css'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <About />
-      <Shop />
-    </div>
+    <Router>  
+      <div>
+        <Nav />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
+      </div>
+    </Router>
   );
 }
 
